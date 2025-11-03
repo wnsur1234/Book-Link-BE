180586ed6c33d860fc5adee67a4756f4c2360dca
scy <tjcksdud0221@naver.com>
2025-11-04 02:57:48 +0900

    :sparkles: feat: 리뷰에 프로필이미지, 등록일자, 닉네임 추가
    리뷰 조회시 반환되는 정보에 프로필이미지, 닉네임, 등록일자를 추가했습니다.

--- src/main/java/com/bookbook/booklink/review_service/model/dto/response/ReviewListDto.java
+++ src/main/java/com/bookbook/booklink/review_service/model/dto/response/ReviewListDto.java
@@ -1,5 +1,6 @@
 package com.bookbook.booklink.review_service.model.dto.response;
 
+import com.bookbook.booklink.auth_service.model.Member;
 import com.bookbook.booklink.review_service.model.Review;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -18,8 +20,11 @@ public class ReviewListDto {
     @Schema(description = "리뷰 ID", example = "550e8400-e29b-41d4-a716-446655440000")
     private UUID reviewId;
 
-    @Schema(description = "작성자 이름", example = "test@test.com")
-    private String reviewerName;
+    @Schema(description = "프로필 이미지 URL", example = "https://example.com/profile.jpg")
+    private String profileImage;
+
+    @Schema(description = "작성자 닉네임", example = "nicknick")
+    private String nickname;
 
     @Schema(description = "별점", example = "4")
     private Short rating;
@@ -27,12 +32,18 @@ public class ReviewListDto {
     @Schema(description = "코멘트", example = "전체적으로 책 상태가 깔끔해요!")
     private String comment;
 
+    @Schema(description = "작성 시간", example = "2025-09-23T20:05:00")
+    private LocalDateTime createdAt;
+
     public static ReviewListDto fromEntity(Review review) {
+        Member reviewer = review.getReviewer();
         return ReviewListDto.builder()
                 .reviewId(review.getId())
-                .reviewerName(review.getReviewer().getName())
+                .profileImage(reviewer.getProfileImage())
+                .nickname(reviewer.getNickname())
                 .rating(review.getRating())
                 .comment(review.getComment())
+                .createdAt(review.getCreateAt())
                 .build();
     }
 }
